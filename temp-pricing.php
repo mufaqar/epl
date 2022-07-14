073a4868134fc31205c96780aa1d693ce477d6f6
Mufaqar Islam <mufaqar@gmail.com>
2022-07-14 12:33:05 +0500

    Price Page

--- temp-pricing.php
+++ temp-pricing.php
@@ -6,6 +6,14 @@
  */
 $banner = get_field('banner');
 $query = $_GET['pricing']; 
+
+
+
+
+
+
+
+
 get_header();
 ?>
  
@@ -31,57 +39,116 @@ get_header();
                                 }                                                    
                             ?>
                         </select>
-                        <button>Search</button>
+                       
                  </form>
             </div>
            </div>
            <div class="new-offer">
                <div class="title">
-                   <h1><?php the_field('pricing_table_heading'); ?></h1>
+                   <h1><?php if($query == '') { echo "All";} 
+                   else {
+                                    $terms_name = get_term_by('slug',$query, 'pricing_types'); 
+
+                                   // print_r($terms_name);
+                                    echo $terms_name->name;
+
+                                   
+                                    
+                        } ?> </h1>
                </div>
                <div class="pricing-cards">
-                   <div class="row"> 
-                    <?php 
+               <div class="row">
 
-                            if($query == '') {
-                                    query_posts(array(
-                                        'post_type' => 'pricing_table',
-                                        'posts_per_page' => -1,
-                                        'order' => 'desc'
-                                        
-                                    ));
-                                }
-                            else {
-                                    query_posts(array(
-                                        'post_type' => 'pricing_table',
-                                        'posts_per_page' => -1,
-                                        'order' => 'desc',
-                                        'pricing_types' => $query
-                                        
-                                    ));
-                                 }
-                    
-                            
+
+                
+                             
+               <?php 
+
+                if($query == '') {
+
+                    $prices_tax = get_terms( array('taxonomy' => 'pricing_types','hide_empty' => false ) ); 
+                    foreach( $prices_tax as $p_cat ) 
+                            {                              
+                                
+                                $cat_name = $p_cat->name ;
+                                $cat_slug = $p_cat->slug ;
+                                echo '<div class="title">';
+                                echo "<h1> $cat_name </h1> </div>";
+
+                                                 
+
+                                ?>
+
+                          
+
+
+                                <div class="row">
+                                        <?php      
+                                    
+                                                        query_posts(array(
+                                                            'post_type' => 'pricing_table',
+                                                            'posts_per_page' => -1,
+                                                            'pricing_types' => $cat_slug,
+                                                            'order' => 'desc'
+                                                            
+                                                        ));
+                                                    if (have_posts()) :  while (have_posts()) : the_post(); ?>
+                                                
+                                                        <div class="col-md-4 card-padding">
+                                                            <div class="card-1">
+                                                                    <h2><?php the_field('price'); ?></h2>
+                                                                    <h3><?php the_title(); ?></h3>
+                                                                    <div class="para">
+                                                                        <?php if( have_rows('list_items') ):  while( have_rows('list_items') ): the_row();   ?> 
+                                                                            <p><?php the_sub_field('item'); ?></p>                                 
+                                                                        <?php endwhile;  endif; ?>
+                                                                    </div>
+                                                                    <button class="btn btn-buy"><a href="<?php the_field('buy_now'); ?>">BUY NOW</a></button>
+                                                            </div>
+                                                        </div>
+                                                    
+                                                    <?php endwhile; wp_reset_query(); else : ?>
+                                                    <h2><?php _e('Nothing Found','lbt_translate'); ?></h2>
+                                        <?php endif;   ?>         
+                                    
+                                    </div>
+               <?php  }  ?>
+               
+
+                <?php }  else {
+
+                            query_posts(array(
+                                'post_type' => 'pricing_table',
+                                'posts_per_page' => -1,
+                                'order' => 'desc',
+                                'pricing_types' => $query
+                                
+                            ));
+
+                
+
                                 if (have_posts()) :  while (have_posts()) : the_post(); ?>
-                                 <div class="col-md-4 card-padding">
-                                      <div class="card-1">
-                                            <h2><?php the_field('price'); ?></h2>
-                                            <h3><?php the_title(); ?></h3>
-                                            <div class="para">
-                                                <?php if( have_rows('list_items') ):  while( have_rows('list_items') ): the_row();   ?> 
-                                                    <p><?php the_sub_field('item'); ?></p>                                 
-                                                <?php endwhile;  endif; ?>
-                                            </div>
-                                            <button class="btn btn-buy"><a href="<?php the_field('buy_now'); ?>">BUY NOW</a></button>
-                                     </div>
-                                 </div>
-                                 
-                                 <?php endwhile; wp_reset_query(); else : ?>
-			                    <h2><?php _e('Nothing Found','lbt_translate'); ?></h2>
-	                <?php endif; ?>         
-                    
-               </div>
-           </div>
+                                                            
+                                                            <div class="col-md-4 card-padding">
+                                                                <div class="card-1">
+                                                                        <h2><?php the_field('price'); ?></h2>
+                                                                        <h3><?php the_title(); ?></h3>
+                                                                        <div class="para">
+                                                                            <?php if( have_rows('list_items') ):  while( have_rows('list_items') ): the_row();   ?> 
+                                                                                <p><?php the_sub_field('item'); ?></p>                                 
+                                                                            <?php endwhile;  endif; ?>
+                                                                        </div>
+                                                                        <button class="btn btn-buy"><a href="<?php the_field('buy_now'); ?>">BUY NOW</a></button>
+                                                                </div>
+                                                            </div>
+                                                            
+                                                            <?php endwhile; wp_reset_query(); else : ?>
+                                                        <h2><?php _e('Nothing Found','lbt_translate'); ?></h2>
+                                <?php endif;  } ?>  
+
+               
+
+           </div></div>
         </div>
     </div>
 <?php
